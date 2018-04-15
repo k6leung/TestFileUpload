b59b3f460b7d4af224118890a27985d25bd90c99
ken <plcnkl@msn.com>
2018-04-15 12:29:06 +0800

    initial commit

---
+++ FileUploadDownloadTest/src/main/java/com/test/file/FileUploadDownloadTestApplication.java
@@ -0,0 +1,12 @@
+package com.test.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadTestApplication.class, args);
+	}
+}
